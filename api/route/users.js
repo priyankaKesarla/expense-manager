8b56d7484a3c01ef7b03a3814d050bc6bc00797e
priyankaKesarla <priyanka.kesarla@gmail.com>
2016-12-18 10:00:20 +0530

    addition of deafult categories

--- api/route/users.js
+++ api/route/users.js
@@ -23,16 +23,32 @@ exports.logout = function(req, res) {
 exports.register = function(req, res) {
  console.log("Bodyyyy"+req.toJson);
    var user = new db.userModel();
-    
-  user.username = req.body.username;
-  user.password = req.body.password;
+  user.username = req.body.userdetails.username;
+  user.password = req.body.userdetails.password;
+    console.log("print user details"+user.username+"dcoij"+user.password);
 
   user.save(function(err) {
-    if (err) {
-      console.log("MY Error" +err);
-      return res.sendStatus(400);
-    }
-
-    return res.sendStatus(200);
+    if(!err){
+          req.body.category.forEach(function(val)
+                                   {
+              var category=new db.categoryModel();
+           category.name = val;
+              console.log("checking................."+category.name);
+	       category.user_id = user._id;
+              category.save(function(err){
+                  if(err){
+                      console.log("MY Error" +err);
+                        return res.sendStatus(400);
+                  }
+              });
+           
+              
+          });
+         return res.sendStatus(200);
+           
+      }
+      console.log("checking.................");
+return res.sendStatus(400);
+   // return res.sendStatus(200);
   });
 }
