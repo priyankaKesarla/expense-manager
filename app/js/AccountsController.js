61e69db11f805300f8ee0f0e9dd16c3c48bc4f42
priyankaKesarla <priyanka.kesarla@gmail.com>
2016-12-18 10:02:21 +0530

    Add files via upload
    updating pie chart functionality

--- app/js/AccountsController.js
+++ app/js/AccountsController.js
@@ -1,56 +1,19 @@
-budgetControllers.controller('AccountListCtrl', ['$scope', '$http',
-	function AccountListCtrl($scope, $http) {
-		$scope.accounts = [];
-
-	    $http.get('http://localhost:4000/accounts').success(function(data) {
-	    	$scope.accounts = data;
-	    });
-
-
-	    $scope.addAccount = function(account) {
-	    	if (account === undefined || account.name == null || account.currency == null) {
-	    		return ;
-	    	}
-	    	//Create the new account with form input values
-	    	var a 		= new Object();
-	    	a.name 		= account.name;
-	    	a.currency 	= account.currency;
-
-	    	//Save Account
-	    	$http.post('http://localhost:4000/accounts', a, {withCredentials: true}).success(function(data) {
-		    	$scope.accounts.push(data);
-		    });
-	    };
-
-	    $scope.deleteAccount = function(accountId) {
-	    	$http.delete('http://localhost:4000/accounts/' + accountId, {withCredentials: true}).success(function(data) {
-	    		var accounts = $scope.accounts;
-		    	for (var accountKey in accounts) {
-		    		if (accounts[accountKey]._id == accountId) {
-		    			$scope.accounts.splice(accountKey, 1);
-		    			return ;
-		    		}
-	    		}
-		    });
-	    };
-
-	}]);
- 
 budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$http', '$location',
 	function AccountDetailCtrl($scope, $routeParams, $http, $location) {
 		$scope.categories 	= [];
-		$scope.account 		= {};
-		var account_id		= $routeParams.accountId;
+        $scope.records 	= [];
 
-		$http.get('http://localhost:4000/accounts/' + account_id, {withCredentials: true}).success(function(data) {
-			$scope.account = data;
-			updateChart();
+		$http.get('http://localhost:4000/records', {withCredentials: true}).success(function(data) {
+			$scope.records = data;
+             updateChart();
+           
 		}).error(function(data, status) {
-			$location.path("/accounts");
-		});
-
+			$location.path("/records");
+		})
+        
 		$http.get('http://localhost:4000/categories', {withCredentials: true}).success(function(data) {
 	    	$scope.categories = data;
+              updateChart();
 	    })
 
 	    $scope.addRecord = function(record) {
@@ -69,37 +32,21 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
 	    	r.description 	= record.description;
 	    	r.date 			= new Date().getTime();
 	    	r.amount 		= amount;
-	    	r.is_expense 	= record.type == 0 ? true : false;
-	    	r.account_id	= account_id;
 
 
 	    	//Save Record
-	    	$http.post('http://localhost:4000/accounts/' + account_id + '/records', r, {withCredentials: true}).success(function(data) {
-	    		if (data.is_expense) {
-	    			$scope.account.balance -= data.amount;
-	    		}
-	    		else {
-	    			$scope.account.balance += data.amount;
-	    		}
-
-		    	$scope.account.records.push(data)
+	    	$http.post('http://localhost:4000/records', r, {withCredentials: true}).success(function(data) {
+		    	$scope.records.push(data)
 		    	updateChart();
 		    });
 	    };
 
 	    $scope.deleteRecord = function(record) {
-	    	$http.delete('http://localhost:4000/accounts/' + account_id + '/records/' + record._id, {withCredentials: true}).success(function(data) {
-		    	var records = $scope.account.records;
+	    	$http.delete('http://localhost:4000/records/' + record._id, {withCredentials: true}).success(function(data) {
+		    	var records = $scope.records;
 		    	for (var recordKey in records) {
 		    		if (records[recordKey]._id == record._id) {
-		    			if (records[recordKey].is_expense) {
-			    			$scope.account.balance += records[recordKey].amount;
-			    		}
-			    		else {
-			    			$scope.account.balance -= records[recordKey].amount;
-			    		}
-			    		
-		    			$scope.account.records.splice(recordKey, 1);
+		    			$scope.records.splice(recordKey, 1);
 		    			updateChart();
 		    		}
 		    	}
@@ -107,11 +54,10 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
 	    };
 
 	    function updateChart() {
-	    	var records = $scope.account.records;
-	    	var totalExpense = 0;
-	    	var totalIncome = 0;
+	    	var records = $scope.records;
             var catgExpense=[];
             var pieData=[];
+             $scope.colors=[];
             function rndColor() {
     var hex = ['0', '1', '2', '3', '4', '5', '6', '7',
                '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
@@ -123,8 +69,12 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
 };
            $scope.categories.forEach(function(val)
                                     {
+               for(var recordKey in records)
+                    {
+                       if(records[recordKey].category==val.name)
+                           {
                catgExpense.push({'category':val.name,'totalAmount':0});
-           });
+                           } }});
             
                
             catgExpense.forEach(function(val)
@@ -141,7 +91,9 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
             });
             catgExpense.forEach(function(val)
                                {
-                pieData.push({'value':val.totalAmount,'color':rndColor()})
+                var cl=rndColor();
+                $scope.colors.push({'color':cl,'category':val.category});
+                pieData.push({'label':val.category, 'value':val.totalAmount,'color':cl})
                 
             });
              
